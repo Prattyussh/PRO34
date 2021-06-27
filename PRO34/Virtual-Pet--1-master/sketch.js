369e5b85ac3c5ab7f271551534f8579f6bc0fb6f
Prattyussh <67913781+Prattyussh@users.noreply.github.com>
2021-06-27 14:01:23 +0530

    Add files via upload

---
+++ PRO34/Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,67 @@
+//Create variables here
+var dog
+var Dogimg 
+var database
+var foodS
+var foodStock
+var dogsitimg
+
+function preload()
+{
+	Dogimg=loadImage("images/dogImg1.png")
+  dogsitimg=loadImage("images/dogImg.png")
+}
+
+function setup() {
+  database=firebase.database()
+
+  createCanvas(800, 700);
+
+  //creating sprite
+  dog=createSprite(250,300,150,150)
+  dog.addImage(dogsitimg)
+  dog.scale=0.15
+
+foodStock=database.ref('food')
+foodStock.on("value",readStock)
+textSize("20")
+
+	
+  
+}
+
+
+function draw() {  
+background(46,139,87)
+
+if (keyWentDown(UP_ARROW)){
+  writeStock(foodS)
+  dog.addImage(Dogimg)
+}
+
+  drawSprites();
+  //add styles here
+  fill(255,255,254);
+   stroke("black");
+    text("Food remaining : "+foodS,170,200);
+     textSize(13);
+      text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+function readStock(data){
+foodS=data.val()
+}
+
+function writeStock(x){
+  if(x<=0){
+   x=0
+  }
+  else{
+    x=x-1
+  }
+database.ref('/').update({
+  food:x
+})
+
+}
+
